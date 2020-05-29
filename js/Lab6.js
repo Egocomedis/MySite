655919791b02bd1c47b162a403fdef9b3cfc2096
Egocomedis <63917319+Egocomedis@users.noreply.github.com>
2020-05-29 12:37:33 +0300

    Add ablablab

---
+++ js/Lab6.js
@@ -0,0 +1,41 @@
+let flag = false;
+let inputVar = document.querySelectorAll('[type="number"]');
+function checkFlag(){
+	for (let i = 0; i < inputVar.length; i++) {
+		if(inputVar[i].value === "")
+		{
+			inputVar[i].placeholder="Введiть число";
+			inputVar[i].style.backgroundColor="#ff0000"
+			flag=false;
+		}
+		else{
+			inputVar[i].style.backgroundColor="#ffffff"
+			flag=true;
+		}
+	}
+}
+
+document.querySelector("#clear").onclick=function(){
+	for (let i = 0; i < inputVar.length; i++) {
+		inputVar[i].style.backgroundColor="#ffffff";
+		inputVar[i].placeholder="Введiть значення";
+		inputVar[i].value="";
+		document.querySelector("#result").textContent="";
+	}
+}
+
+function solver(){
+	checkFlag();
+	document.querySelector("#result").textContent="Сума непарних чисел від а до b дорівнює " + calc() +  " цифр"
+}
+
+function calc(){
+	let sum = 0;
+	for(let i = inputVar[0].value; i <= inputVar[1].value; i++){
+			//let n = arr[i];
+			if(i % 2 == 1)
+				sum++;
+	}
+	return sum;
+}
+
